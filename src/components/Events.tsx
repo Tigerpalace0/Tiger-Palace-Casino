6dc9d90bf8261f18b4c5a253de1f9bfe12ad4a0c
unknown <vanuchdry07@gmail.com>
2025-12-17 18:24:01 +0545

    month

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -44,7 +44,7 @@ const Events: React.FC = () => {
 
   const upcomingEvents = [
     {
-      date: 'August',
+      date: 'Currently Running',
       title: ' Pole Dance',
       time: '09:00 PM',
       status: 'Available',
